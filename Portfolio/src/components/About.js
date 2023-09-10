0e7e99940083e4993932924d023813e01dce2f77
Chinmay Satish Vyawahare <121102640+chinmay0703@users.noreply.github.com>
2023-09-10 19:08:52 +0530

    Add files via upload

---
+++ Portfolio/src/components/About.js
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react'
+
+function About() {
+    return (
+        <Fragment>
+            <div className="col py-3 d-flex align-items-center about-image">
+
+                <div className="container">
+                    <div className="row">
+
+
+
+
+                        <div className="col-lg-6 col-md-12 col-sm-12 d-flex align-items-center">
+                            <div>
+                                <h1 className="font-big text-red slide-in-top">About me</h1>
+                                <hr></hr>
+                                <p className='slide-in-bottom'>Ambitious full-stack developer, a self-led learner and team player  <br></br>
+
+                                </p>
+                                <p className='slide-in-bottom'>
+                                    To secure a challenging position in a leading industry that recognizes and
+                                    rewards excellence in teamwork and provides opportunities to further my
+                                    education and expand my skill set. With a post-graduate degree from CDAC,
+                                    I have developed a strong foundation in software development and possess
+                                    expertise in various programming languages and frameworks.<br></br>
+                                    to work on challenging and diverse projects with the latest technologies.
+                                </p>
+                                <p className='slide-in-bottom'>
+                                    
+                                    The technology I've worked on : <span className="text-red ls-2">Java , Spring-Boot , React.js , Javascript<br></br>
+                                        My-SQL , Html , Css , Bootstrap </span>
+                                </p>
+                                <p className='slide-in-bottom'>
+                                    Currently Learning Angular Framework.
+                                </p>
+                            </div>
+                        </div>
+                        <div className="col-lg-6 col-md-12 col-sm-12">
+                            <img className="img-fluid" src="../images/c.jpg" alt="..."></img>
+                        </div>
+
+
+
+                    </div>
+                </div>
+
+
+            </div>
+        </Fragment>
+    )
+}
+
+export default About
